990223857e30bf68621b8d4a05a31d5822292b32
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-04-15 01:17:31 +0530

    getAreaSP

---
+++ soicodata_azure/dbo/Stored Procedures/usp_getArea.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[usp_getArea]
+	@thisHId int 
+	
+AS
+
+DECLARE @CurrentHId hierarchyid
+
+SELECT @CurrentHId = EntitiyHierarchyId FROM EntitiyHierarchy WHERE EntityId = @thisHId ;
+
+SELECT * FROM EntitiyHierarchy WHERE EntitiyHierarchyId.IsDescendantOf(@CurrentHId) = 1 AND EntityType = 3 ;
+
