2f04c122792c0f63172f7e6839242f3cf6bed65d
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-04-15 00:39:28 +0530

    sp

---
+++ soicodata_azure/dbo/Stored Procedures/getCities.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[getCities]
+	@thisHId int 
+	
+AS
+
+DECLARE @CurrentHId hierarchyid
+
+SELECT @CurrentHId = EntitiyHierarchyId FROM EntitiyHierarchy WHERE EntityId = @thisHId ;
+
+SELECT * FROM EntitiyHierarchy WHERE EntitiyHierarchyId.IsDescendantOf(@CurrentHId) = 1 AND EntityType = 2 ;
+
