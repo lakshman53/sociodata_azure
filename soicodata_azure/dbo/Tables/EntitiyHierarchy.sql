d137ce70c7bb6e2b793a6700596bfd24631b274c
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-04-15 02:55:04 +0530

    sps

--- soicodata_azure/dbo/Tables/EntitiyHierarchy.sql
+++ soicodata_azure/dbo/Tables/EntitiyHierarchy.sql
@@ -2,6 +2,7 @@
     [EntitiyHierarchyId] [sys].[hierarchyid] NOT NULL,
     [EntityId]          INT                 NOT NULL,
     [EntityType]         INT                 NOT NULL,
+    [EntityName] NVARCHAR(50) NULL, 
     PRIMARY KEY CLUSTERED ([EntitiyHierarchyId] ASC)
 );
 
