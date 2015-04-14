f3f29a6b2f3980fd1b8227f7ace826be4e10e856
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-04-15 01:08:42 +0530

    sps all

---
+++ soicodata_azure/dbo/Stored Procedures/usp_addArea.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[usp_AddArea] (@level2id int, @level3id int)
+AS 
+BEGIN
+   DECLARE @mEntitiyHierarchyId hierarchyid, @lc hierarchyid
+   
+   SELECT @mEntitiyHierarchyId = EntitiyHierarchyId 
+   FROM EntitiyHierarchy 
+   WHERE EntityId = @level2id
+   SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+   
+   BEGIN TRANSACTION
+	  SELECT @lc = max(EntitiyHierarchyId) 
+      FROM EntitiyHierarchy 
+      WHERE EntitiyHierarchyId.GetAncestor(1) = @mEntitiyHierarchyId ;
+      
+	  INSERT EntitiyHierarchy (EntitiyHierarchyId, EntityId, EntityType)
+      VALUES(@mEntitiyHierarchyId.GetDescendant(@lc, NULL), @level3id, 3)
+   COMMIT
+
+END;
+GO
